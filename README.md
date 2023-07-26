4a47f4b9398d0232b7f195326e7188034e9de79b
Ryan Li <ryan@Ryans-MacBook-Pro.local>
2023-07-26 14:22:48 -0700

    Base skeleton and updated README setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CZBiohub_Takehome
+Written using Next.js framework. <br>
+If you don’t have Node.js installed, install it from [here](https://nodejs.org/en/). You’ll need Node.js version 10.13 or later. <br>
+
+## Spin up
+run "npm run dev" and visit "http://localhost:3000"
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main>
+        <h1 className={styles.title}>
+          CZ BioHub Takehome Assignment
+        </h1>
+
+        <p className={styles.description}>
+          Ryan Li
+        </p>
+      </main>
+    </div>
+  )
+}
