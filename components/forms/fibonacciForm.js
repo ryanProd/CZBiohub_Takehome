9089b634d6eb67c07fc01a4029966393a181c071
Ryan Li <ryan@Ryans-MacBook-Pro.local>
2023-07-26 23:18:54 -0700

    Form input validation

--- components/forms/fibonacciForm.js
+++ components/forms/fibonacciForm.js
@@ -2,15 +2,36 @@ import { useRouter } from 'next/router'
 
 export default function FibonacciForm() {
     const router = useRouter()
-
+    
     const handleSubmit = async (event) => {
 
         event.preventDefault();
 
         const form = event.target;
+        const val = form.fibInput.value;
+
+        if (!val) {
+            alert('Please enter value');
+            return false;
+        }
+
+        if (isNaN(Number(val))){
+            alert('Please enter a number');
+            return false;
+        }
+
+        if (!Number.isInteger(Number(val))){
+            alert('Please enter an integer');
+            return false;
+        }
+
+        if (Number(val) < 0){
+            alert('Please enter a positive number');
+            return false;
+        }
 
         const data = {
-            fibInput: form.fibInput.value
+            fibInput: val
         };
 
         const response = await fetch('/api/form', {
@@ -24,7 +45,6 @@ export default function FibonacciForm() {
         });
 
         const result = await response.json();
-        //alert(`Check: ${result.data}`)
         if (result != null){
             router.push({
                 pathname: '/result',
